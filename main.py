8433da842f7552cfed2246d08d509f47082bcac4
OllieD27 <gdosku20@gmail.com>
2026-03-08 17:33:31 +0100

    Change ENV DB

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI",'sqlite:///posts.db')
 app.config['GRAVATAR_DEFAULT_IMAGE'] = 'identicon'  # Default image if no Gravatar is found
 app.config['GRAVATAR_SIZE'] = 100
 db = SQLAlchemy(model_class=Base)
